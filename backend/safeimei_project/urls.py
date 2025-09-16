91219d270a8313a72d6e55acc3b164bf39d99c06
devwithshams <talktoshamsuddeen.com>
2025-09-16 15:04:52 +0100

    CHORE: Add and pass all tests for the portal app

--- backend/safeimei_project/urls.py
+++ backend/safeimei_project/urls.py
@@ -4,6 +4,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# --- START: ADD THESE LINES FOR ADMIN CUSTOMIZATION ---
+admin.site.site_header = "SafeIMEI Administration"
+admin.site.site_title = "SafeIMEI Admin Portal"
+admin.site.index_title = "Welcome to the SafeIMEI Portal"
+# --- END: ADD THESE LINES ---
 
 urlpatterns = [
     path('admin/', admin.site.urls),
